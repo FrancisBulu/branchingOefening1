f19f7406677623ff86d863b0017dfcce233bf9c5
Francis Bulu <FrancisB@cursisten.intecbrussel.intra>
2020-10-08 16:00:27 +0200

    Add project files.

---
+++ branchingOefening1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace branchingOefening1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een getal in:");
+            int getal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef een tweede getal in:");
+            int getal2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"De twee getalen zijn: { getal1} en { getal2}");
+
+
+            switch (getal1 > getal2)
+            {
+                case true:
+                    Console.WriteLine($"De stijgende volgorde is: {getal2}, {getal1}");
+                    break;
+                case false:
+                    Console.WriteLine($"De stijgende volgorde is: {getal1}, {getal2}");
+                    break;
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
